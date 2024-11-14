cfb6bba8d7d84afd0bc115680603a9edbb5ec741
Hatice Taşdemir <tdemirhtc@gmail.com>
2024-11-14 15:29:38 +0300

    Initial Commit

---
+++ LandmarkSUI/LandmarkSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkSUIApp.swift
+//  LandmarkSUI
+//
+//  Created by Hatice Taşdemir on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
